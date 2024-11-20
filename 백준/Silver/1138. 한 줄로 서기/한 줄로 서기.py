3aba44b8adc6a4df93755682428b65498f67633b
dxxwls <75723636+eunjin0468@users.noreply.github.com>
2024-11-20 21:36:49 +0900

    [Silver II] Title: 한 줄로 서기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+answer = [0]*n
+
+for i in range(n):
+    cnt = 0
+    for j in range(n):
+        if answer[j] == 0 and cnt == arr[i]:
+            answer[j] = i+1
+            break
+        elif answer[j] ==0:
+            cnt+=1
+print(' '.join(map(str, answer)))
