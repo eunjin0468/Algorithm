a3db3dba367aa0cc59426340475f516a9c1678b3
dxxwls <75723636+eunjin0468@users.noreply.github.com>
2024-11-27 17:26:45 +0900

    [Bronze III] Title: 네 번째 점, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,11 @@
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+x3, y3 = map(int, input().split())
+x_list = [x1, x2, x3]
+y_list = [y1, y2, y3]
+for x in x_list:
+    if x_list.count(x)==1:
+        print(x, end=' ')
+for y in y_list:
+    if y_list.count(y)==1:
+        print(y)
