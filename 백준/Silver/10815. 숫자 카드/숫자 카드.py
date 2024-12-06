5be4f4222ae579f6f9f4f77185439f1d13b90dce
dxxwls <75723636+eunjin0468@users.noreply.github.com>
2024-12-06 19:18:42 +0900

    [Silver V] Title: 숫자 카드, Time: 3524 ms, Memory: 108796 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr_1 = list(map(int, input().split()))
+arr_1.sort()
+m = int(input())
+arr_2 = list(map(int, input().split()))
+
+for num in arr_2:
+    left, right = 0,n-1
+    isExist = False
+    while left <= right:
+        mid = (left+right)//2
+        if num == arr_1[mid]:
+            isExist=True
+            print(1, end=' ')
+            break
+        elif num>arr_1[mid]:
+            left = mid+1
+        else:
+            right = mid-1
+    if not isExist:
+        print(0, end=' ')
