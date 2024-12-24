cc58feddb261804f21962abe77043c48b8bcace5
dxxwls <75723636+eunjin0468@users.noreply.github.com>
2024-12-24 19:57:22 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,17 @@
+a = int(input())
+for _ in range(a):
+    str = input()
+    if str == "Algorithm":
+        print(204)
+    elif str == "DataAnalysis":
+        print(207)
+    elif str == "ArtificialIntelligence":
+        print(302)
+    elif str == "CyberSecurity":
+        print("B101")
+    elif str =="Network":
+        print(303)
+    elif str=="Startup":
+        print(501)
+    elif str=="TestStrategy":
+        print(105)
