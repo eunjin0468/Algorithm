69dc135bad339b519ba0a49e80fb0eeb48fee6db
dxxwls <75723636+eunjin0468@users.noreply.github.com>
2025-02-28 17:13:13 +0900

    [Silver III] Title: 1로 만들기, Time: 452 ms, Memory: 40224 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,15 @@
+n = int(input())
+d = [0]*1000001
+
+for i in range(2, n+1):
+    #현재의 수-1
+    d[i] = d[i-1]+1
+
+    #현재의 수//2
+    if i%2==0:
+        d[i] = min(d[i], d[i//2]+1)
+
+    #현재의 수//3
+    if i%3==0:
+        d[i] = min(d[i], d[i//3]+1)
+print(d[n])
