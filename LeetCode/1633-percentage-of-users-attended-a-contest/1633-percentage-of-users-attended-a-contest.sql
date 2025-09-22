dc00fbea341df16f7dcba47e33a2124a31221098
dxxwls <75723636+eunjin0468@users.noreply.github.com>
2025-09-22 09:11:28 +0900

    Time: 808 ms (99.41%), Space: 0B (100%) - LeetHub

--- LeetCode/1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ LeetCode/1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,4 +1,4 @@
-select r.contest_id,  round(count(u.user_id)/(select count(*) from users)*100,2) as percentage
+select r.contest_id, round(count(u.user_id)/(select count(*) from users)*100,2) as percentage
 from users u
 join register r on u.user_id=r.user_id
 group by r.contest_id 
