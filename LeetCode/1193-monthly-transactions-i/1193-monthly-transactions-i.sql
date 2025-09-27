969186ad140fc449d45c1459d5df44ce70710a92
dxxwls <75723636+eunjin0468@users.noreply.github.com>
2025-09-27 22:07:56 +0900

    Time: 622 ms (36.02%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,8 @@
+select left(trans_date, 7) as month, 
+        country, 
+        count(id) as trans_count, 
+        sum(case state when 'approved' then 1 else 0 end) as approved_count, 
+        sum(amount) as trans_total_amount, 
+        sum(case state when 'approved' then amount else 0 end) as approved_total_amount
+from transactions
+group by country, month;
